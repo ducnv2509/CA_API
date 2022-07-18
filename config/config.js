b994e60537a22ebd7b65eb23c166b33ba55ea921
ducnv2509 <ducit2509@gmail.com>
2022-07-18 10:27:13 +0700

    up config

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export const HOST = 'localhost';
+export const USER = 'sqluser';
+export const PASSWORD = '123456';
+export const DB_NAME = 'db_ca';
+export const PORTDB = 3306;
+export const POOL = {
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+};
