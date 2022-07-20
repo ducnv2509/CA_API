14d69a601a36ce438d1bea44c7ba241769888c7a
ducnv2509 <ducit2509@gmail.com>
2022-07-20 08:54:27 +0700

    fix_acces

--- controllers/LoginByStaff.js
+++ controllers/LoginByStaff.js
@@ -11,9 +11,9 @@ export async function loginByStaff(username, password) {
         let ret = result[0][0];
         let { res, id, full_name, email, phone, role } = ret;
         if (res == 1) {
-            let accsessToken = genTokenStaff(username, full_name, email, phone, role);
+            let accsesToken = genTokenStaff(username, full_name, email, phone, role);
             let refreshToken = genRefreshTokenStaff(username, full_name, email, phone, role);
-            return { statusCode: 200, data: { full_name, email, phone, accsessToken, refreshToken, role } }
+            return { statusCode: 200, data: { full_name, email, phone, accsesToken, refreshToken, role } }
         } else {
             return { statusCode: 401, error: 'USERNAME_NOT_FOUND', description: 'username not found' };
         }
