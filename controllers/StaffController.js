cebf771989c8fea4268d446fd29e08ccdff4c67a
ducnv2509 <ducit2509@gmail.com>
2022-07-20 18:41:57 +0700

    loading 90%

--- controllers/StaffController.js
+++ controllers/StaffController.js
@@ -1,5 +1,6 @@
 import myLogger from "../winstonLog/winston.js";
 import query from "../helper/helperDb.js";
+import { BAD_REQUEST } from "../constant/HttpResponseCode.js";
 
 export async function updateTicketByStaff(account_name, project_id, group_id, priority_id, scope, summary, description_by_staff, assignee_id, status_id, request_type_id, sizing_id, id) {
     let params = [account_name, project_id, group_id, priority_id, scope, summary, description_by_staff, assignee_id, status_id, request_type_id, sizing_id, id]
@@ -23,9 +24,12 @@ export async function updateTicketStatusByStaff(ticket_id, created_by_account, n
     try {
         const result = await query(sql, params);
         let ret = result[0][0];
-        console.log(ret);
         let id = ret.res;
-        return { statusCode: 200, data: { ticket_id, created_by_account, new_status, note, date_activity, time_spent } };
+        if (id > 0) {
+            return { statusCode: 200, data: { ticket_id, created_by_account, new_status, note, date_activity, time_spent } };
+        } else {
+            return { statusCode: BAD_REQUEST, error: 'UPDATE_FALSE', description: 'update false' };
+        }
     } catch (error) {
         myLogger.info("login e: %o", error);
         return { statusCode: 500, error: 'ERROR', description: 'System busy!' };
@@ -39,9 +43,12 @@ export async function updateTransferTicketByStaff(ticket_id, new_group, new_assi
     try {
         const result = await query(sql, params);
         let ret = result[0][0];
-        console.log(ret);
         let id = ret.res;
-        return { statusCode: 200, data: { ticket_id, new_group, new_assignee, time_spent, note, date_of_activity, create_by_account } };
+        if (id > 0) {
+            return { statusCode: 200, data: { ticket_id, created_by_account, new_status, note, date_activity, time_spent } };
+        } else {
+            return { statusCode: BAD_REQUEST, error: 'UPDATE_FALSE', description: 'update false' };
+        }
     } catch (error) {
         myLogger.info("login e: %o", error);
         return { statusCode: 500, error: 'ERROR', description: 'System busy!' };
@@ -56,7 +63,11 @@ export async function updateCommentByStaff(ticket_id, created_by_account, note, 
         let ret = result[0][0];
         console.log(ret);
         let id = ret.res;
-        return { statusCode: 200, data: { ticket_id, created_by_account, note, date_activity, time_spent } };
+        if (id > 0) {
+            return { statusCode: 200, data: { ticket_id, created_by_account, new_status, note, date_activity, time_spent } };
+        } else {
+            return { statusCode: BAD_REQUEST, error: 'UPDATE_FALSE', description: 'update false' };
+        }
     } catch (error) {
         myLogger.info("login e: %o", error);
         return { statusCode: 500, error: 'ERROR', description: 'System busy!' };
@@ -178,7 +189,7 @@ export async function createTicketByStaff(account_name,
 
 export async function getDetailsTicket(ticket_id, account_name) {
     let params = [ticket_id]
-    let paramsLog = [ account_name, ticket_id]
+    let paramsLog = [account_name, ticket_id]
     let sql = `CALL getAllTicketById(?)`
     let sqlLog = `CALL getLogCommentByTicket(?,?)`
     try {
@@ -186,7 +197,7 @@ export async function getDetailsTicket(ticket_id, account_name) {
         const resultLog = await query(sqlLog, paramsLog);
         let ret = result[0];
         let retLog = resultLog[0];
-       
+
         let details = [];
         let detailsLog = [];
         ret.forEach(e => {
@@ -212,8 +223,8 @@ export async function getTimeSpent(account_name) {
         let ret = result[0];
         let details = [];
         ret.forEach(e => {
-            let { count0,count1,count2,count3,count4,count5,count6,count7 } = e;
-            details.push({ count0,count1,count2,count3,count4,count5,count6,count7 });
+            let { count0, count1, count2, count3, count4, count5, count6, count7 } = e;
+            details.push({ count0, count1, count2, count3, count4, count5, count6, count7 });
         })
         return { statusCode: 200, data: { details } };
     } catch (error) {
