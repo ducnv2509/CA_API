a53db019a7bb55bd5b18f0dbd26f99923cd83839
ducnv2509 <ducit2509@gmail.com>
2022-08-02 16:44:30 +0700

    update email

--- controllers/StaffController.js
+++ controllers/StaffController.js
@@ -28,7 +28,7 @@ export async function updateTransferTicketNew(
         };
         let projectRes = await fetch(constUrl, requestOptions)
             .then(response => response.json());
-        // await sendMail(assigneeName + '@fpt.com.vn', note, issue_key, assigneeName)
+        await sendMail(assigneeName + '@fpt.com.vn', note, issue_key, assigneeName)
         myLogger.info(JSON.stringify(projectRes))
         let params = [ticket_id, new_group, assigneeName, time_spent, note, date_activity, created_by_account]
         let sql = `CALL transferTicketByStaff(?, ?, ?, ?, ?, ?, ?)`
